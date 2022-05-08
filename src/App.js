73384c981a43884a858748c634ae4047e277cb1f
iuliabrezoi <65541409+iuliabrezoi@users.noreply.github.com>
2022-05-08 13:23:57 +0300

    Prima parte - strucutra

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPage from './JavaS/Componente/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
